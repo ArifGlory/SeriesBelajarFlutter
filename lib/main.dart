faec7af47fdeb004f9d49c266a9759ced4385d98
ArifGlory <arifglory46@gmail.com>
2023-03-08 14:23:42 +0700

    udah belajar sampe staeful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,44 +6,73 @@ main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  List<Widget> myList = [
-    Container(
-      width: 300,
-      height: 200,
-      color: Colors.amber,
-    ),
-    Container(
-      width: 300,
-      height: 200,
-      color: Colors.blue,
-      margin: EdgeInsets.all(3),
-    ),
-    Container(
-      width: 50,
-      height: 200,
-      color: Colors.red,
-      margin: EdgeInsets.all(3),
-    ),
-    Container(
-      width: 300,
-      height: 200,
-      color: Colors.purpleAccent,
-      margin: EdgeInsets.all(3),
-    ),
-  ];
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 1;
 
   @override
   Widget build(BuildContext context) {
+
+
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("List View"),
+          title: Text("Stateful"),
         ),
-        body: ListView(
-          children: myList,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              counter.toString(),
+              style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: () {
+                  setState(() {
+                    counter++;
+                  });
+                },
+                    child: Icon(Icons.add)),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        counter--;
+                      });
+                    },
+                    child: Icon(Icons.remove))
+              ],
+            )
+          ],
         ),
       ),
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+
+  const ChatItem(
+      {required this.imageUrl, required this.title, required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing: Text("10:00"),
     );
   }
 }
