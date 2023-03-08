91b43793b562259da94e414cc4cfdbe8c61aab0f
ArifGlory <arifglory46@gmail.com>
2023-03-08 15:41:52 +0700

    belajar mapping list

--- lib/main.dart
+++ lib/main.dart
@@ -13,43 +13,77 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int counter = 1;
+  final List<Map<String,dynamic>> myList = [
+    {
+      "Name" : "Arif",
+      "Age" : 25,
+      "favColor" : ["Navy","Green","Red","Green","Red","Green","Red","Green","Red","Green","Red"]
+    },{
+      "Name" : "Glory",
+      "Age" : 26,
+      "favColor" : ["Violet","Green","Red","Green","Red","Green","Red","Green","Red"]
+    },
+    {
+      "Name" : "Rahman",
+      "Age" : 27,
+      "favColor" : ["Yellow","Green","Red","Green","Red"]
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-
-
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Stateful"),
+          title: Text("mapping List"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              counter.toString(),
-              style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(onPressed: () {
-                  setState(() {
-                    counter++;
-                  });
-                },
-                    child: Icon(Icons.add)),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        counter--;
-                      });
-                    },
-                    child: Icon(Icons.remove))
-              ],
-            )
-          ],
+        body: ListView(
+          children: myList.map((data){
+            List myColor = data["favColor"];
+            return Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 10
+              ),
+              child: Container(
+                margin: EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(),
+                        Column(
+                          children: [
+                            Text(" Name : ${data['Name']}"),
+                            Text(" Age : ${data['Age']}"),
+                          ],
+                        )
+                      ],
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: myColor.map((warna){
+                          return Container(
+                            color: Colors.greenAccent,
+                            child: Text(warna),
+                            margin: EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 10
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 10
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
